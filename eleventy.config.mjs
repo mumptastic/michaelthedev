42c46b490b493a87867cecaaa0824d205d3b9624
Michael Nichols <mnichols@tamu.edu>
2025-06-17 08:13:52 -0500

    UPDATE: Moved to Eleventy

---
+++ eleventy.config.mjs
@@ -0,0 +1,39 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import  syntaxHighlight  from "@11ty/eleventy-plugin-syntaxhighlight";
+
+
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets/**/*");
+
+    eleventyConfig.addCollection("sortedNotes", function(collectionApi) {
+        return collectionApi.getFilteredByTag("note")
+            .filter(item => !item.data.draft)
+            .sort((a, b) => {
+            return new Date(b.data.date) - new Date(a.data.date);
+        });
+    });
+
+
+    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "atom", // or "rss", "json"
+        outputPath: "/feed.xml",
+        collection: {
+            name: "note",
+            limit: 0,
+        },
+        metadata: {
+            language: "en",
+            title: "Michael The Developer",
+            subtitle: "My little side of the internet",
+            base: "https://michaelthe.dev",
+            author: {
+                name: "Michael Nichols",
+                email: "me@michaelthe.dev", // Optional
+            }
+        }
+    });
+
+
+};
+
---
+++ notes/note-1.md
@@ -0,0 +1,7 @@
+---
+title: Note One!
+date: 2025-06-01
+draft: false
+---
+
+This is my first note!
